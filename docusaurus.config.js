0d4bff644adf0c1a97fc6ec5edf472187e6794c0
emartindev <emartindev@gmail.com>
2021-09-17 12:52:32 -0400

    updated config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,13 +5,13 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 module.exports = {
   title: 'BIG Docs',
   tagline: 'Documents live here',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://emmettmartinbig.github.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Big Digital', // Usually your GitHub org/user name.
+  projectName: 'Big Documentation', // Usually your repo name.
   themeConfig: {
     navbar: {
       title: 'BIG Docs',
